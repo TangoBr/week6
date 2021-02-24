585dccbecd22a3f052546aa3649a17655450582c
TangoBr <thomasbridges01@gmail.com>
2021-02-23 22:45:16 -0500

    learning

--- hw1.js
+++ hw1.js
@@ -1,4 +1,14 @@
-var navbar = document.getElementById('navbar');
-var showcase = document.getElementById('showcase');
-var footer = document.getElementById('footer');
-var footerNodes =
+var navbar = document.getElementById('navbar'); //3
+console.log(navbar);
+var showcase = document.getElementById('showcase'); //4
+console.log(showcase);
+var footer = document.getElementById('footer'); //5
+console.log(footer);
+var footerNodes = document.querySelectorAll('footer'); //6
+console.log(footerNodes) 
+// var footerChildren =                                //7
+var navLinks = document.getElementsByClassName('nav-link'); //8
+console.log(navLinks);
+var ul = document.querySelector('ul'); //9
+console.log(ul)
+// var aboutLi =
